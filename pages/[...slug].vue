4437494e53440977015942ecc4a74d446aea1c7a
chawata <cha39881@gmail.com>
2024-11-08 02:24:50 +0900

    add: dayjs

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -1,9 +1,14 @@
+<script setup lang="ts">
+import dayjs from 'dayjs'
+const formatDate = (date: string) => dayjs(date).format('YYYY/MM/DD')
+</script>
+
 <template>
   <ContentDoc>
       <template v-slot="{ doc }">
         <article>
-          <p>作成日: {{ doc.created_at }}</p>
-          <p>更新日: {{ doc.updated_at }}</p>
+          <p>作成日: {{ formatDate(doc.created_at) }}</p>
+          <p>更新日: {{ formatDate(doc.updated_at) }}</p>
           <ContentRenderer :value="doc" />
         </article>
       </template>
